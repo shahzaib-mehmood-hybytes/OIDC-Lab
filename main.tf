cdfdbaf613542cab2fb6770d2d3dff6e85b30a90
shahzaib-mehmood-hybytes <shahzaib.mehmood@hybytes.com>
2025-10-20 17:45:31 +0500

    okokok

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Define the S3 bucket resource
 resource "aws_s3_bucket" "my_example_bucket" {
-  bucket = "OIDC-SHZB0011" # Replace with a globally unique bucket name
+  bucket = "oidc-shzb0011" # Replace with a globally unique bucket name
 
   tags = {
     Name        = "MyExampleS3Bucket"
